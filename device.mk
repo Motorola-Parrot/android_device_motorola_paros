57989f5a8fc3e418517918433855762d6afaf981
AMWolfstein <ahmedhero4040@gmail.com>
2025-07-12 01:27:09 +0300

    paros: Import overlays based on stock ROM
    * From paros:15/SUQ32HV-V1-ST14/56b832:user/release-keys
    
    Change-Id: I483c26f69a4e94ce9944049e8702135ce72a9f24

--- device.mk
+++ device.mk
@@ -40,6 +40,12 @@ $(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/etc/init/*.rc),\
 $(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/bin/*.sh),\
         $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/bin/$(notdir $f)))
 
+# Overlays
+PRODUCT_PACKAGES += \
+    FrameworksResDevice \
+    SystemUIResDevice \
+    WifiResDevice
+
 # NFC
 TARGET_USES_NXP_NFC := true
 
